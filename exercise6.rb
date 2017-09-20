a8931fce2dc40c6cc2d6cfce2ca271742b7466fc
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-19 23:01:31 -0400

    added round to 1 decimal to ex 6

--- exercise6.rb
+++ exercise6.rb
@@ -3,7 +3,7 @@ puts "Tell me what temperature in Fahrenheit you want me to convert to Celcius"
 fahr = gets.to_i
 
 def f2c(fahr)
-  cel = (fahr - 32)*(5/9.0)
+  cel = ((fahr - 32)*(5/9.0)).round(1)
 end
 
   puts "The temperature in Celcius is #{f2c(fahr)}"
