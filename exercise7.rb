77aa737c64fa36ab9c1247f860593344926df0f0
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-19 22:55:36 -0400

    ex 7 completed

--- exercise7.rb
+++ exercise7.rb
@@ -1,5 +1,7 @@
 def wrap_text(st1, st2)
-  return "#{st1} + #{st2}"
+  return "#{st2.reverse + st1 + st2}"
 end
 
-puts wrap_text("a", "b")
+puts wrap_text("hello", "===")
+
+puts wrap_text("new message", "###===---")
