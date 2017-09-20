58c4aba6425ac97bf4d3930fa0f5cf557b062702
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-19 22:59:32 -0400

    cleaned up code on ex 7 and 6

--- exercise6.rb
+++ exercise6.rb
@@ -3,8 +3,7 @@ puts "Tell me what temperature in Fahrenheit you want me to convert to Celcius"
 fahr = gets.to_i
 
 def f2c(fahr)
-  cel = (fahr - 32)*(5.0/9.0)
-  puts "The temperature in Celcius is #{cel}"
+  cel = (fahr - 32)*(5/9.0)
 end
 
-puts f2c(fahr)
+  puts "The temperature in Celcius is #{f2c(fahr)}"
